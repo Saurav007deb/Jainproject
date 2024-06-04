6c1eed73e175a30fe8d13b947a0bfaf1205e3a4a
Saurav <contactdeb07@gmail.com>
2024-06-05 01:31:17 +0530

    type module

--- merncomapp/src/index.js
+++ merncomapp/src/index.js
@@ -1,8 +1,8 @@
-const React = require('react');
-const ReactDOM = require('react-dom/client');
-require('./index.css');
-const App = require('./App');
-const reportWebVitals = require('./reportWebVitals');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
